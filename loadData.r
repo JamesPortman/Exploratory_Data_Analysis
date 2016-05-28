8e79150c19634e48e8772abb40281d0e8a4a8b01
James Portman <james@portman.ca>
2016-05-28 12:35:05 -0400

    Update loadData.r

--- loadData.r
+++ loadData.r
@@ -1,3 +1,12 @@
-# load_date.r
+# loadData.R
+## Description: Load in data required for answering Project questions
+## # Date: May 28, 2016
+## Author: James Portman
 
-## load the required data for further processing
+setwd("/Users/admin/ExploratoryProject2/")
+
+## Create the NEI and SCC data frames from the .rds files.
+df_NEI <- readRDS("summarySCC_PM25.rds")
+df_SCC <- readRDS("Source_Classification_Code.rds")
+
+## End loadData.R
